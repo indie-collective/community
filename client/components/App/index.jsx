4706d3eeacdc4970911b2fe05136218babc45daf
Christopher Humphries <engleek@gmail.com>
2019-02-25 11:50:39 +0100

    Moving URQL provider to top of component tree

--- client/components/App/index.jsx
+++ client/components/App/index.jsx
@@ -1,42 +1,14 @@
 import React from 'react';
-import {
-  createClient,
-  cacheExchange,
-  fetchExchange,
-  Provider,
-} from 'urql';
 
-import fileExchange from './fileExchange';
 import Navigation from '../Navigation';
 
-const host = (location.host.split(':')[0] === 'localhost') ? 'localhost:4000' : location.host;
-
-const client = createClient({
-  url: `${location.protocol}//${host}/graphql`,
-  exchanges: [
-    cacheExchange,
-    fileExchange,
-    fetchExchange,
-  ],
-  fetchOptions() {
-    if (localStorage.key('token')) {
-      return {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      };
-    }
-    return {};
-  },
-});
-
 const App = ({ children }) => (
-  <Provider value={client}>
+  <div>
     <Navigation />
     <div style={{ paddingTop: 90 }}>
       {children}
     </div>
-  </Provider>
+  </div>
 );
 
 export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Redirect } from '@reach/router';
+import {
+  createClient,
+  cacheExchange,
+  fetchExchange,
+  Provider,
+} from 'urql';
 
 import StructuresPage from './pages/structures';
 import EventsPage from './pages/events';
@@ -10,16 +16,43 @@ import PeoplePage from './pages/people';
 import StructurePage from './pages/structure';
 import EventDialog from './pages/event';
 
+import Navigation from './components/Navigation';
+
+import fileExchange from './fileExchange';
+
+const host = (location.host.split(':')[0] === 'localhost') ? 'localhost:4000' : location.host;
+
+const client = createClient({
+  url: `${location.protocol}//${host}/graphql`,
+  exchanges: [
+    cacheExchange,
+    fileExchange,
+    fetchExchange,
+  ],
+  fetchOptions() {
+    if (localStorage.key('token')) {
+      return {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      };
+    }
+    return {};
+  },
+});
+
 render(
-  <Router>
-    <Redirect from="/" to="/structures" />
-    <StructuresPage path="/structures" />
-    <EventsPage path="/events">
-      <EventDialog path=":id" />
-    </EventsPage>
-    <GamesPage path="/games" />
-    <PeoplePage path="/people" />
-    <StructurePage path="/structure/:id" />
-  </Router>,
+  <Provider value={client}>
+    <Router>
+      <Redirect from="/" to="/structures" />
+      <StructuresPage path="/structures" />
+      <EventsPage path="/events">
+        <EventDialog path=":id" />
+      </EventsPage>
+      <GamesPage path="/games" />
+      <PeoplePage path="/people" />
+      <StructurePage path="/structure/:id" />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
