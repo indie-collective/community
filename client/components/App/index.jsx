a3a24d4da8cd7e14fdbed81b0311f2b4aeaf6e00
Christopher Humphries <engleek@gmail.com>
2019-07-06 15:26:22 +0200

    Move routes to own file

--- client/components/App/index.jsx
+++ client/components/App/index.jsx
@@ -1,51 +1,8 @@
 import React from 'react';
 import { useRoutes } from 'hookrouter';
 
-import HomePage from '../../pages/home';
-
-import GamesPage from '../../pages/games';
-import GamePage from '../../pages/game';
-import CreateGame from '../../pages/createGame';
-
-import EntitiesPage from '../../pages/entities';
-import EntityPage from '../../pages/entity';
-import CreateEntity from '../../pages/createEntity';
-
-import PeoplePage from '../../pages/people';
-import PersonPage from '../../pages/person';
-import CreatePerson from '../../pages/createPerson';
-
-import EventsPage from '../../pages/events';
-import EventPage from '../../pages/event';
-import CreateEvent from '../../pages/createEvent';
-
-import TagsPage from '../../pages/tags';
-import TagPage from '../../pages/tag';
-
-import Navigation from 'components/Navigation';
-
-const routes = {
-  '/': () => <HomePage />,
-
-  '/games': () => <GamesPage />,
-  '/game/create': () => <CreateGame />,
-  '/game/:id': ({ id }) => <GamePage id={Number.parseInt(id)} />,
-
-  '/entities': () => <EntitiesPage />,
-  '/entities/create': () => <CreateEntity />,
-  '/entities/:id': ({ id }) => <EntityPage id={Number.parseInt(id)} />,
-
-  '/people': () => <PeoplePage />,
-  '/person/create': () => <CreatePerson />,
-  '/person/:id': ({ id }) => <PersonPage id={Number.parseInt(id)} />,
-
-  '/events': () => <EventsPage />,
-  '/event/create': () => <CreateEvent />,
-  '/event/:id': ({ id }) => <EventPage id={Number.parseInt(id)} />,
-
-  '/tags': () => <TagsPage />,
-  '/tag/:id': ({ id }) => <TagPage id={Number.parseInt(id)} />,
-};
+import routes from '../../routes';
+import Navigation from '/components/Navigation';
 
 const App = () => {
   const routeResult = useRoutes(routes);
