67c1954fe91311161cb4885cc78f40682f4269af
Christopher Humphries <engleek@gmail.com>
2019-07-16 19:28:16 +0200

    Draft AutoSelect component

---
+++ client/components/AutoSelect/AutoSelect.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import AutoSelect from '.';
+
+storiesOf('Organisms/AutoSelect', module)
+  .add('default', () => (
+    <div style={{ width: 300, padding: 15 }}>
+      <AutoSelect onSubmit={action('onSubmit')} />
+    </div>
+  ));
