28eca0ce2fc9cd0fe722899007cee708b04a9320
Christopher Humphries <engleek@gmail.com>
2019-12-29 11:56:09 +0100

    feat: new tab based navigation

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,98 +1,34 @@
-import Link from 'next/link';
-import {
-  Box,
-  Flex,
-  Button,
-  ButtonGroup,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-} from '@chakra-ui/core';
+import { Box, Flex, Tabs, TabList, Tab } from '@chakra-ui/core';
 import { useRouter } from 'next/router';
+
 import Logo from './Logo';
 
-const CategoryMenu = () => (
-  <Menu>
-    <MenuButton as={Button} h="1.75rem" size="sm" rightIcon="chevron-down">
-      All
-    </MenuButton>
-    <MenuList placement="auto-end">
-      <MenuItem>Games</MenuItem>
-      <MenuItem>Orgs</MenuItem>
-      <MenuItem>Events</MenuItem>
-    </MenuList>
-  </Menu>
-);
-
-const SearchBar = () => {
-  const [show, setShow] = React.useState(false);
-
-  return (
-    <InputGroup size="md">
-      <Input pr="5rem" type="text" placeholder="Search" />
-      <InputRightElement width="4rem" mr=".5em">
-        <CategoryMenu />
-      </InputRightElement>
-    </InputGroup>
-  );
-};
+const tabs = [
+  { label: 'Games', url: '/games' },
+  { label: 'Orgs', url: '/orgs' },
+  { label: 'Events', url: '/events' },
+];
 
 const Navigation = () => {
-  const { pathname } = useRouter();
+  const { pathname, push } = useRouter();
 
   return (
-    <Flex p="10px">
-      <Link href="/">
-        <a>
-          <Logo />
-        </a>
-      </Link>
+    <Flex pl={5} pt={5} alignItems="end">
+      <Logo />
 
       <Box flex="auto" />
 
-      <ButtonGroup>
-        <Link href="/games">
-          <Button
-            variantColor="blue"
-            variant="ghost"
-            isActive={pathname === '/games'}
-            mr={2}
-          >
-            Games
-          </Button>
-        </Link>
-
-        <Link href="/orgs">
-          <Button
-            variantColor="blue"
-            variant="ghost"
-            isActive={pathname === '/orgs'}
-            mr={2}
-          >
-            Orgs
-          </Button>
-        </Link>
-
-        <Link href="/events">
-          <Button
-            variantColor="blue"
-            variant="ghost"
-            isActive={pathname === '/events'}
-          >
-            Events
-          </Button>
-        </Link>
-      </ButtonGroup>
-
-      <ButtonGroup p="10px" display="none">
-        <Button variantColor="red" variant="ghost">
-          Login
-        </Button>
-      </ButtonGroup>
+      <Tabs
+        variant="solid-rounded"
+        onChange={i => push(tabs[i].url)}
+        index={tabs.findIndex(tab => tab.url === pathname)}
+      >
+        <TabList>
+          {tabs.map(tab => (
+            <Tab key={tab.index}>{tab.label}</Tab>
+          ))}
+        </TabList>
+      </Tabs>
     </Flex>
   );
 };
