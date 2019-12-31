b3c73cadc8240c1535dc8aaa6ce849548f32bdcd
Christopher Humphries <engleek@gmail.com>
2019-12-31 14:49:51 +0100

    feat: add signin form

---
+++ client/components/SigninForm.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+} from '@chakra-ui/core';
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email()
+    .required(),
+  password: yup.string().required(),
+});
+
+const propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+const SigninForm = ({ onSubmit }) => {
+  const { handleSubmit, register, errors } = useForm({
+    validationSchema,
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl mb={5} isInvalid={errors.email} isRequired>
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <Input name="email" ref={register} />
+        <FormErrorMessage>
+          {errors.email && errors.email.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl mb={5} isInvalid={errors.password} isRequired>
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input name="password" type="password" ref={register} />
+        <FormErrorMessage>
+          {errors.password && errors.password.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <Button type="submit" mt={3} variantColor="green">
+        Sign In
+      </Button>
+    </form>
+  );
+};
+
+SigninForm.propTypes = propTypes;
+
+export default SigninForm;
