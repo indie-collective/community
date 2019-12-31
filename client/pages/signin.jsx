f3b89b0c2d792e3912a96564659cc5174c7ca5af
Christopher Humphries <engleek@gmail.com>
2019-12-31 15:20:18 +0100

    feat: connect graphql mutations to signup / signin pages

--- client/pages/signin.jsx
+++ client/pages/signin.jsx
@@ -1,12 +1,26 @@
 import { Box, Heading } from '@chakra-ui/core';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
 
 import SigninForm from '../components/SigninForm';
 
-export default () => (
-  <Box width={500} margin="40px auto">
-    <Heading mb={5}>Sign Up</Heading>
-    <Box border="1px solid #eee" mb={10} p={3} borderRadius={5}>
-      <SigninForm onSubmit={console.log} />
+const signinMutation = gql`
+  mutation signin($email: String!, $password: String!) {
+    authenticate(input: { email: $email, password: $password }) {
+      jwtToken
+    }
+  }
+`;
+
+export default () => {
+  const [signin, { data, loading }] = useMutation(signinMutation);
+
+  return (
+    <Box width={500} margin="40px auto">
+      <Heading mb={5}>Sign In</Heading>
+      <Box border="1px solid #eee" mb={10} p={3} borderRadius={5}>
+        <SigninForm onSubmit={variables => signin({ variables })} />
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
--- client/pages/signup.jsx
+++ client/pages/signup.jsx
@@ -1,12 +1,43 @@
 import { Box, Heading } from '@chakra-ui/core';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
 
 import SignupForm from '../components/SignupForm';
 
-export default () => (
-  <Box width={500} margin="40px auto">
-    <Heading mb={5}>Sign Up</Heading>
-    <Box border="1px solid #eee" mb={10} p={3} borderRadius={5}>
-      <SignupForm onSubmit={console.log} />
+const signupMutation = gql`
+  mutation signup(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    registerPerson(
+      input: {
+        firstName: $firstName
+        lastName: $lastName
+        email: $email
+        password: $password
+      }
+    ) {
+      person {
+        id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+export default () => {
+  const [signin, { data, loading }] = useMutation(signupMutation);
+
+  return (
+    <Box maxWidth={500} margin="40px auto">
+      <Heading mb={5}>Sign Up</Heading>
+      <Box border="1px solid #eee" mb={10} p={3} borderRadius={5}>
+        <SignupForm onSubmit={variables => signin({ variables })} />
+      </Box>
+      {data && <pre>{JSON.stringify(data, false, 2)}</pre>}
     </Box>
-  </Box>
-);
+  );
+};
