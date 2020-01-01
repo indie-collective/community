d500cd34c3345db7618af7639469121297c6c44d
Christopher Humphries <engleek@gmail.com>
2020-01-01 21:57:58 +0100

    feat: show welcome message on successful signup

--- client/pages/signup.jsx
+++ client/pages/signup.jsx
@@ -1,6 +1,7 @@
-import { Box, Heading } from '@chakra-ui/core';
 import { gql } from 'apollo-boost';
 import { useMutation } from '@apollo/react-hooks';
+import { Box, Heading, Text, Button } from '@chakra-ui/core';
+import NextLink from 'next/link';
 
 import SignupForm from '../components/SignupForm';
 
@@ -29,13 +30,30 @@ const signupMutation = gql`
 `;
 
 export default () => {
-  const [signin, { data, loading }] = useMutation(signupMutation);
+  const [signin, { data, loading, error }] = useMutation(signupMutation);
+
+  if (!loading && !error && data) {
+    return (
+      <Box maxWidth={500} margin="40px auto">
+        <Heading mb={4}>
+          Hi there, {data.registerPerson.person.firstName}!
+        </Heading>
+        <Text fontSize="xl">Welcome to the community!</Text>
+        <NextLink href="/signin">
+          <Button color="teal.500" mt="24px">Sign in</Button>
+        </NextLink>
+      </Box>
+    );
+  }
 
   return (
     <Box maxWidth={500} margin="40px auto">
       <Heading mb={5}>Sign Up</Heading>
       <Box border="1px solid #eee" mb={10} p={3} borderRadius={5}>
-        <SignupForm onSubmit={variables => signin({ variables })} />
+        <SignupForm
+          loading={loading}
+          onSubmit={variables => signin({ variables })}
+        />
       </Box>
       {data && <pre>{JSON.stringify(data, false, 2)}</pre>}
     </Box>
