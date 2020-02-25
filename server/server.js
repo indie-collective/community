d6fececd0ee9bb443b9a799aa5297ecf2e83f719
Alexis Launay <yorunohikage@gmail.com>
2020-02-25 01:34:55 +0100

    Add S3 to upload image

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { postgraphile } = require('postgraphile');
 const { graphqlUploadExpress } = require('graphql-upload');
 const jimp = require('jimp');
-const expressPlayground = require('graphql-playground-middleware-express').default
+const AWS = require('aws-sdk');
+const expressPlayground = require('graphql-playground-middleware-express')
+  .default;
 
 const PostGraphileUploadFieldPlugin = require('postgraphile-plugin-upload-field');
 const PostGraphileDerivedFieldPlugin = require('postgraphile-plugin-derived-field');
@@ -14,16 +15,31 @@ const ConnectionFilterPlugin = require('postgraphile-plugin-connection-filter');
 
 const { jwtSecret } = require('./config.json');
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 require('dotenv').config();
 
-const {DB_USER, DB_PASS} = process.env;
+const {
+  DB_USER,
+  DB_PASS,
+  S3_ACCESS_KEY_ID,
+  S3_SECRET_ACCESS_KEY,
+} = process.env;
 
 const DB_URL =
   DB_USER && DB_PASS
     ? `postgres://${DB_USER}:${DB_PASS}@localhost:5432/indieco`
     : 'postgres://localhost:5432/indieco';
 
-const thisServerUrl = isDev ? 'http://localhost:4000' : 'https://community.indieco.xyz';
+const s3 = new AWS.S3({
+  endpoint: 's3.fr-par.scw.cloud',
+  region: 'fr-par',
+  accessKeyId: S3_ACCESS_KEY_ID,
+  secretAccessKey: S3_SECRET_ACCESS_KEY,
+  signatureVersion: 'v4',
+  // s3ForcePathStyle: true,
+  params: { Bucket: 'community' },
+});
 
 const app = express();
 
@@ -63,76 +79,121 @@ app.use(
       ],
       derivedFieldDefinitions: [
         {
-          identifiers: [{
-            columns: 'image_file',
-          }],
+          identifiers: [
+            {
+              columns: 'image_file',
+            },
+          ],
           inflect: fieldName => 'url',
           resolve: image => {
             if (image) {
-              return `${thisServerUrl}${IMAGES_PATH}/${image.name}`;
+              try {
+                return s3.getSignedUrl('getObject', {
+                  Bucket: 'community',
+                  Key: image.name,
+                  Expires: 900,
+                });
+              } catch (e) {
+                console.log(e);
+              }
             }
+            return null;
           },
         },
         {
-          identifiers: [{
-            columns: 'image_file',
-          }],
+          identifiers: [
+            {
+              columns: 'image_file',
+            },
+          ],
           inflect: fieldName => 'thumbnail_url',
           resolve: image => {
             if (image) {
-              return `${thisServerUrl}${IMAGES_PATH}/thumb_${image.name}`;
+              try {
+                return s3.getSignedUrl('getObject', {
+                  Bucket: 'community',
+                  Key: `thumb_${image.name}`,
+                  Expires: 900,
+                });
+              } catch (e) {
+                console.log(e);
+              }
             }
+            return null;
           },
-        }
+        },
       ],
     },
   })
 );
 
-if (!(DB_USER && DB_PASS)) {
-  app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+if (isDev) {
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
 }
 
 app.listen(4000, () => {
   console.log('Server listening on port 4000');
 });
 
+const storeUpload = async ({ object, filename }) => {
+  const upload = s3.putObject({
+    ACL: 'public-read',
+    Key: filename,
+    Body: object,
+  });
+
+  return upload.promise();
+};
+
 async function resolveUpload(upload) {
   const { filename, createReadStream } = upload;
   const stream = createReadStream();
 
-  const localFilename = await saveLocal({ stream, filename });
-  const image = await jimp.read(
-    path.join(process.cwd(), UPLOAD_DIR_NAME, localFilename)
-  );
-  const { width, height } = image.bitmap;
+  if (!stream) {
+    throw new Error(
+      'File is not defined, did you forget to use Content-Type: multipart/form-data?'
+    );
+  }
 
-  await image.resize(jimp.AUTO, 400);
-  await image.writeAsync(
-    path.join(process.cwd(), UPLOAD_DIR_NAME, `thumb_${localFilename}`)
-  );
-
-  return {
-    name: localFilename,
-    width,
-    height,
-    thumb_width: image.bitmap.width,
-    thumb_height: image.bitmap.height,
-  };
-}
-
-function saveLocal({ stream, filename }) {
   const timestamp = new Date().toISOString().replace(/\D/g, '');
-  const localFilename = `${timestamp}.${filename.split('.').pop()}`;
-  const fsPath = path.join(process.cwd(), UPLOAD_DIR_NAME, localFilename);
+  const newFilename = `${timestamp}.${filename.split('.').pop()}`;
 
-  return new Promise((resolve, reject) =>
-    stream
-      .on('error', error => {
-        if (stream.truncated) fs.unlinkSync(fsPath);
-        reject(error);
-      })
-      .on('end', () => resolve(localFilename))
-      .pipe(fs.createWriteStream(fsPath))
-  );
+  const chunks = [];
+  const imageBuffer = await new Promise((resolve, reject) => {
+    stream.on('data', chunk => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+  });
+
+  try {
+    const { ETag, VersionId } = await storeUpload({
+      object: imageBuffer,
+      filename: newFilename,
+    });
+
+    const image = await jimp.read(imageBuffer);
+    const { width, height } = image.bitmap;
+
+    await image.resize(jimp.AUTO, 400);
+    const buffer = await image.getBufferAsync(jimp.AUTO);
+
+    const { ETag: thumbETag, VersionId: thumbVersionId } = await storeUpload({
+      object: buffer,
+      filename: `thumb_${newFilename}`,
+    });
+
+    return {
+      name: newFilename,
+      ETag,
+      VersionId,
+      width,
+      height,
+      thumbETag,
+      thumbVersionId,
+      thumb_width: image.bitmap.width,
+      thumb_height: image.bitmap.height,
+    };
+  } catch (err) {
+    throw new Error('Something wrong occurred when uploading');
+  }
 }
