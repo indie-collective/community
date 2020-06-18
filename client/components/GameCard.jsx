76cde8ea9830fe2f4657fdef51213113827110bd
Alexis Launay <yorunohikage@gmail.com>
2020-06-19 00:39:58 +0200

    Reformat GameCard

--- client/components/GameCard.jsx
+++ client/components/GameCard.jsx
@@ -13,117 +13,119 @@ import {
 
 import usePlaceholder from '../hooks/usePlaceholder';
 
-const GameCard = forwardRef(({ id, images, name, onRemove, isCompact }, ref) => {
-  const { colorMode } = useColorMode();
-  const placeholder = usePlaceholder();
+const GameCard = forwardRef(
+  ({ id, images, name, onRemove, isCompact }, ref) => {
+    const { colorMode } = useColorMode();
+    const placeholder = usePlaceholder();
 
-  const bgColorHover = colorMode === 'dark' ? 'gray.700' : 'gray.50';
+    const bgColorHover = colorMode === 'dark' ? 'gray.700' : 'gray.50';
 
-  if (isCompact) {
-    const overlayBgColor = colorMode === 'dark' ? '#00000088' : '#ffffffbb';
+    if (isCompact) {
+      const overlayBgColor = colorMode === 'dark' ? '#00000088' : '#ffffffbb';
+
+      return (
+        <Link href="/game/[id]" as={`/game/${id}`}>
+          <a>
+            <PseudoBox
+              rounded={5}
+              transition="background-color 200ms ease-out"
+              ref={ref}
+              position="relative"
+              overflow="hidden"
+              border="1px solid"
+              borderColor={bgColorHover}
+              _hover={{
+                backgroundColor: bgColorHover,
+                cursor: 'pointer',
+              }}
+            >
+              {images.length > 0 && (
+                <Image
+                  position="absolute"
+                  objectFit="cover"
+                  rounded="md"
+                  size="100%"
+                  src={images[0].thumbnail_url}
+                  alt=""
+                  zIndex={-1}
+                />
+              )}
+
+              <PseudoBox
+                display="flex"
+                rounded="md"
+                paddingY={2}
+                paddingX={5}
+                zIndex={2}
+                backgroundColor={images.length > 0 ? overlayBgColor : ''}
+                fontWeight="bold"
+              >
+                <Text>{name}</Text>
+
+                <IconButton
+                  m="auto"
+                  ml={2}
+                  size="xs"
+                  aria-label={`Remove ${name}`}
+                  isRound
+                  variantColor="red"
+                  icon="delete"
+                  onClick={(e) => {
+                    e.preventDefault();
+
+                    onRemove();
+                  }}
+                  tabIndex="-1"
+                />
+              </PseudoBox>
+            </PseudoBox>
+          </a>
+        </Link>
+      );
+    }
 
     return (
       <Link href="/game/[id]" as={`/game/${id}`}>
         <a>
           <PseudoBox
-            rounded={5}
-            transition="background-color 200ms ease-out"
             ref={ref}
-            position="relative"
-            overflow="hidden"
-            border="1px solid"
-            borderColor={bgColorHover}
+            boxShadow="0 1px 2px rgba(0, 0, 0, 0.1)"
             _hover={{
               backgroundColor: bgColorHover,
               cursor: 'pointer',
             }}
+            rounded={5}
           >
-            {images.length > 0 && (
+            <AspectRatioBox ratio={2}>
               <Image
-                position="absolute"
-                objectFit="cover"
-                rounded="md"
                 size="100%"
-                src={images[0].thumbnail_url}
-                alt=""
-                zIndex={-1}
+                objectFit="cover"
+                src={images.length > 0 && images[0].thumbnail_url}
+                alt="Game cover"
+                fallbackSrc={placeholder}
+                rounded="md"
               />
-            )}
+            </AspectRatioBox>
 
-            <PseudoBox
-              display="flex"
-              rounded="md"
-              paddingY={2}
-              paddingX={5}
-              zIndex={2}
-              backgroundColor={images.length > 0 ? overlayBgColor : ''}
-              fontWeight="bold"
-            >
-              <Text>{name}</Text>
-
-              <IconButton
-                m="auto"
-                ml={2}
-                size="xs"
-                aria-label={`Remove ${name}`}
-                isRound
-                variantColor="red"
-                icon="delete"
-                onClick={(e) => {
-                  e.preventDefault();
-
-                  onRemove();
-                }}
-                tabIndex="-1"
-              />
-            </PseudoBox>
+            <Box padding={2}>{name}</Box>
           </PseudoBox>
+
+          <IconButton
+            right={0}
+            top="-25%"
+            size="xs"
+            aria-label={`Remove ${name}`}
+            isRound
+            position="absolute"
+            variantColor="red"
+            icon="delete"
+            onClick={onRemove}
+            tabIndex="-1"
+          />
         </a>
       </Link>
     );
   }
-
-  return (
-    <Link href="/game/[id]" as={`/game/${id}`}>
-      <a>
-        <PseudoBox
-          ref={ref}
-          boxShadow="0 1px 2px rgba(0, 0, 0, 0.1)"
-          _hover={{
-            backgroundColor: bgColorHover,
-            cursor: 'pointer',
-          }}
-          rounded={5}
-        >
-          <AspectRatioBox ratio={2}>
-            <Image
-              size="100%"
-              objectFit="cover"
-              src={images.length > 0 && images[0].thumbnail_url}
-              alt="Game cover"
-              fallbackSrc={placeholder}
-              rounded="md"
-            />
-          </AspectRatioBox>
-
-          <Box padding={2}>{name}</Box>
-        </PseudoBox>
-
-        <IconButton
-          right={0}
-          top="-25%"
-          size="xs"
-          aria-label={`Remove ${name}`}
-          isRound
-          position="absolute"
-          variantColor="red"
-          icon="delete"
-          onClick={onRemove}
-          tabIndex="-1"
-        />
-      </a>
-    </Link>
-  );
-});
+);
 
 export default GameCard;
