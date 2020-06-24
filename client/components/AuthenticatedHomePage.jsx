44fd1856c38c2ba81060c1c517004a02c8d0c346
Alexis Launay <yorunohikage@gmail.com>
2020-06-24 22:56:46 +0200

    Change Homepage and add custom data when logged in

--- client/components/AuthenticatedHomePage.jsx
+++ client/components/AuthenticatedHomePage.jsx
@@ -1,36 +1,40 @@
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
-import {
-  Box,
-  Spinner,
-  Heading,
-  Text,
-  Grid,
-  StatGroup,
-  StatLabel,
-  StatNumber,
-  Stat,
-  Image,
-} from '@chakra-ui/core';
+import { Box, Spinner, Heading, Text, Grid, Image } from '@chakra-ui/core';
 import Head from 'next/head';
 import { startOfDay } from 'date-fns';
 import { motion } from 'framer-motion';
 
-import { withApollo } from '../lib/apollo';
-import useCurrentPerson from '../hooks/useCurrentPerson';
-import Navigation from '../components/Navigation';
 import GameCard from '../components/GameCard';
 import OrgCard from '../components/OrgCard';
 import EventCard from '../components/EventCard';
-import AuthenticatedHomePage from '../components/AuthenticatedHomePage';
 import noEventsImage from '../assets/undraw_festivities_tvvj.svg';
 
-const homeQuery = gql`
+const authenticatedHomeQuery = gql`
   ${GameCard.fragments.game}
   ${OrgCard.fragments.org}
   ${EventCard.fragments.event}
 
-  query home($currentDay: Datetime!) {
+  query authenticatedHome($currentDay: Datetime!) {
+    currentPerson {
+      id
+      firstName
+      avatar {
+        url
+      }
+      eventsToCome: joinedEvents(
+        first: 8
+        orderBy: STARTS_AT_DESC
+        filter: { endsAt: { greaterThanOrEqualTo: $currentDay } }
+      ) {
+        nodes {
+          id
+          startsAt
+          ...EventCardEvent
+        }
+      }
+    }
+
     games(first: 12, orderBy: CREATED_AT_DESC) {
       totalCount
       nodes {
@@ -81,82 +85,62 @@ const variants = {
   },
 };
 
-const LandingPage = () => {
-  const { loading, error, data } = useQuery(homeQuery, {
+const AuthenticatedHomePage = () => {
+  const { data, loading } = useQuery(authenticatedHomeQuery, {
     variables: {
       currentDay: startOfDay(new Date()),
     },
   });
 
   if (loading) {
-    return (
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        height="100%"
-      >
-        <Spinner size="lg" />
-      </Box>
-    );
+    return <Spinner />;
   }
 
-  const { games, entities, events, eventsToCome } = data;
+  const { games, entities, eventsToCome } = data;
+  const { firstName, eventsToCome: joinedEventsToCome } = data.currentPerson;
 
   return (
     <Box p={5} mb={5}>
       <Head>
         <title>Indie Collective - Community powered video game data</title>
-        <meta
-          name="description"
-          content="Video game related events around you and all over the world."
-        />
-        <meta property="og:title" content="Indie Collective - Community powered video game data" />
-        <meta
-          property="og:description"
-          content="Video game related events around you and all over the world."
-        />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@IndieColle" />
-        <meta name="twitter:title" content="Indie Collective - Community powered video game data" />
-        <meta
-          name="twitter:description"
-          content="Video game related events around you and all over the world."
-        />
       </Head>
 
-      <Box py={5} mb={10} textAlign="center">
-        <Box m="auto" width={['auto', 'auto', '75%', '50%']}>
-          <Heading mb={5} size="2xl">
-            Find Your Community
+      <Heading size="2xl" mb={10}>
+        Welcome back {firstName}!
+      </Heading>
+
+      {joinedEventsToCome.nodes.length > 0 && (
+        <Box mb={10}>
+          <Heading as="h3" size="xl" mb={5}>
+            Your upcoming events
           </Heading>
 
-          <Text mb={5}>
-            Community lets you explore indie game-focused data around the world.
-            <br />
-            Discover who, where and what has been / is being made near you
-            through crowd-sourced data.
-          </Text>
-
-          <StatGroup my={2}>
-            <Stat>
-              <StatNumber>{games.totalCount}</StatNumber>
-              <StatLabel>games</StatLabel>
-            </Stat>
-
-            <Stat>
-              <StatNumber>{entities.totalCount}</StatNumber>
-              <StatLabel>orgs</StatLabel>
-            </Stat>
-
-            <Stat>
-              <StatNumber>{events.totalCount}</StatNumber>
-              <StatLabel>events</StatLabel>
-            </Stat>
-          </StatGroup>
+          <motion.div
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            variants={{ enter: { transition: { staggerChildren: 0.1 } } }}
+          >
+            <Grid
+              gap={3}
+              templateColumns={[
+                '1fr',
+                'repeat(2, 1fr)',
+                'repeat(3, 1fr)',
+                'repeat(4, 1fr)',
+              ]}
+            >
+              {joinedEventsToCome.nodes.map((event) => (
+                <Box key={event.id} minW={0}>
+                  <motion.div variants={variants}>
+                    <EventCard {...event} />
+                  </motion.div>
+                </Box>
+              ))}
+            </Grid>
+          </motion.div>
         </Box>
-      </Box>
+      )}
 
       <Box mb={10}>
         <Heading as="h3" size="xl" mb={5}>
@@ -264,16 +248,4 @@ const LandingPage = () => {
   );
 };
 
-const Home = () => {
-  const currentPerson = useCurrentPerson();
-
-  return (
-    <Box>
-      <Navigation />
-
-      {currentPerson ? <AuthenticatedHomePage /> : <LandingPage />}
-    </Box>
-  );
-};
-
-export default withApollo()(Home);
+export default AuthenticatedHomePage;
