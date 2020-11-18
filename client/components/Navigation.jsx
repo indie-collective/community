bf15bc81b8fca6e88dfbb9e33bdf577e4cec7d97
Alexis Launay <yorunohikage@gmail.com>
2020-11-18 21:26:44 +0100

    Add search page, revert to simple input

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,15 +1,10 @@
-import {
-  Box,
-  Flex,
-  Tabs,
-  TabList,
-  Tab,
-} from '@chakra-ui/core';
+import { Box, Flex, Tabs, TabList, Tab } from '@chakra-ui/core';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 import Logo from './Logo';
 import AvatarButton from './AvatarButton';
+import SearchInput from './SearchInput';
 
 const tabs = [
   { label: 'Games', url: '/games' },
@@ -17,29 +12,33 @@ const tabs = [
   { label: 'Events', url: '/events' },
 ];
 
-const Navigation = () => {
+const Navigation = ({ search }) => {
   const { pathname, push } = useRouter();
 
   return (
     <Flex pl={5} pr={2} pt={5} alignItems="center">
       <Link href="/">
-        <a><Logo /></a>
+        <a>
+          <Logo />
+        </a>
       </Link>
 
       <Tabs
         ml={5}
         variant="solid-rounded"
-        onChange={i => push(tabs[i].url)}
-        index={tabs.findIndex(tab => tab.url === pathname)}
+        onChange={(i) => push(tabs[i].url)}
+        index={tabs.findIndex((tab) => tab.url === pathname)}
       >
         <TabList>
-          {tabs.map(tab => (
+          {tabs.map((tab) => (
             <Tab key={tab.url}>{tab.label}</Tab>
           ))}
         </TabList>
       </Tabs>
 
       <Box flex="auto" />
+
+      <SearchInput defaultValue={search} />
 
       <AvatarButton />
     </Flex>
---
+++ client/pages/search.jsx
@@ -0,0 +1,182 @@
+import { gql, useQuery } from '@apollo/client';
+import { Box, Spinner, Heading, Text } from '@chakra-ui/core';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
+
+import Navigation from '../components/Navigation';
+import GameCard from '../components/GameCard';
+import OrgCard from '../components/OrgCard';
+import EventCard from '../components/EventCard';
+import Carousel from '../components/Carousel';
+import { withApollo } from '../lib/apollo';
+import { useRouter } from 'next/dist/client/router';
+
+const searchQuery = gql`
+  ${GameCard.fragments.game}
+  ${OrgCard.fragments.org}
+  ${EventCard.fragments.event}
+
+  query search(
+    $gameTokens: [GameFilter!]!
+    $orgTokens: [EntityFilter!]!
+    $eventTokens: [EventFilter!]!
+  ) {
+    games(filter: { or: $gameTokens }, first: 10) {
+      nodes {
+        id
+        ...GameCardGame
+      }
+    }
+
+    orgs: entities(filter: { or: $orgTokens }, first: 10) {
+      nodes {
+        id
+        ...OrgCardOrg
+      }
+    }
+
+    events(filter: { or: $eventTokens }, first: 10) {
+      nodes {
+        id
+        startsAt
+        ...EventCardEvent
+      }
+    }
+  }
+`;
+
+const variants = {
+  initial: { scale: 0.96, y: 30, opacity: 0 },
+  enter: {
+    scale: 1,
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.5, ease: [0.48, 0.15, 0.25, 0.96] },
+  },
+  exit: {
+    scale: 0.6,
+    y: 100,
+    opacity: 0,
+    transition: { duration: 0.2, ease: [0.48, 0.15, 0.25, 0.96] },
+  },
+};
+
+const SearchPage = () => {
+  const router = useRouter();
+
+  const search = router.query.q;
+
+  const tokens = search
+    ? search.split(' ').map((token) => ({
+        name: {
+          likeInsensitive: `%${token}%`,
+        },
+      }))
+    : [];
+  const { data, loading } = useQuery(searchQuery, {
+    variables: {
+      gameTokens: tokens,
+      orgTokens: tokens,
+      eventTokens: tokens,
+    },
+  });
+
+  const hasNoResults =
+    loading ||
+    !data ||
+    (data.games.nodes.length === 0 &&
+      data.orgs.nodes.length === 0 &&
+      data.events.nodes.length === 0);
+
+  return (
+    <Box>
+      <Navigation search={search} />
+
+      <Box p={5} mb={5}>
+        <Head>
+          <title>
+            {search && <>Results for "{search}" | </>}
+            Search
+          </title>
+        </Head>
+
+        {search ? (
+          <>
+            <Heading as="h2" size="2xl" mb={5}>
+              Search results for "{search}"
+            </Heading>
+
+            {loading && <Spinner />}
+            {!loading && hasNoResults && <Text>No results found.</Text>}
+            {!loading && !hasNoResults && (
+              <>
+                {data.games.nodes.length > 0 && (
+                  <Box mb={10}>
+                    <Heading as="h3" size="xl" mb={5}>
+                      Games
+                    </Heading>
+
+                    <Carousel slidesToShow={[1, 2, 3]}>
+                      {data.games.nodes.map((game) => (
+                        <Box key={game.id} minW={0} pr={3}>
+                          <motion.div variants={variants}>
+                            <GameCard {...game} />
+                          </motion.div>
+                        </Box>
+                      ))}
+                    </Carousel>
+                  </Box>
+                )}
+
+                {data.orgs.nodes.length > 0 && (
+                  <Box mb={10}>
+                    <Heading as="h3" size="xl" mb={5}>
+                      Organizations
+                    </Heading>
+
+                    <Carousel slidesToShow={[1, 2, 3]}>
+                      {data.orgs.nodes.map((org) => (
+                        <Box key={org.id} minW={0} pr={3}>
+                          <motion.div variants={variants}>
+                            <OrgCard {...org} />
+                          </motion.div>
+                        </Box>
+                      ))}
+                    </Carousel>
+                  </Box>
+                )}
+
+                {data.events.nodes.length > 0 && (
+                  <Box mb={10}>
+                    <Heading as="h3" size="xl" mb={5}>
+                      Events
+                    </Heading>
+
+                    <Carousel slidesToShow={[1, 2, 3]}>
+                      {data.events.nodes.map((event) => (
+                        <Box key={event.id} minW={0} pr={3}>
+                          <motion.div variants={variants}>
+                            <EventCard {...event} />
+                          </motion.div>
+                        </Box>
+                      ))}
+                    </Carousel>
+                  </Box>
+                )}
+              </>
+            )}
+          </>
+        ) : (
+          <Box textAlign="center" p={20}>
+            <Text color="gray.600" fontSize="3xl">
+              Games, organizations and events matching your research will show
+              up here once you start typing.
+            </Text>
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default withApollo({ ssr: true })(SearchPage);
