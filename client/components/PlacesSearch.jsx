0d576237a1d0cca08d128e690931add532ab54ec
Alexis Launay <yorunohikage@gmail.com>
2021-04-04 19:19:00 +0200

    Fix PlacesSearch because Berlin is a city-region

--- client/components/PlacesSearch.jsx
+++ client/components/PlacesSearch.jsx
@@ -35,41 +35,38 @@ const PlacesSearch = ({ onChange, onClear, options, value, ...rest }) => {
 
         let countryCode, region, city, street;
 
-        if (place.place_type.includes('country'))
-          countryCode = place.short_code;
-        else {
-          countryCode = place.context.find((c) => c.id.includes('country'))
-            .short_code;
-
-          if (place.place_type.includes('region')) region = place.text;
-          else {
-            region = place.context.find((c) => c.id.includes('region')).text;
-
-            if (place.place_type.includes('place')) city = place.text;
-            else {
-              city = place.context.find((c) => c.id.includes('place')).text;
-
-              if (place.place_type.includes('address')) {
-                if (place.address) street = place.address + ' ' + place.text;
-                else street = place.text;
-              } else {
-                const address = place.context.find((c) =>
-                  c.id.includes('address')
-                );
-
-                if (address.address) street = address.address + ' ' + address.text;
-                else street = address.text;
-              }
-            }
+        place.place_type.map(type => {
+          switch (type) {
+            case 'country': countryCode = place.short_code; break;
+            case 'region': region = place.text; break;
+            case 'place': city = place.text; break;
+            case 'address':
+              if (place.address) street = place.address + ' ' + place.text;
+              else street = place.text;
+            break;
+            default: break;
           }
-        }
+        });
+
+        place.context.map(context => {
+          if (context.id.includes('country')) countryCode = context.short_code;
+
+          if (context.id.includes('region')) region = context.text;
+
+          if (context.id.includes('place')) city = context.text;
+
+          if (context.id.includes('address')) {
+            if (address.address) street = address.address + ' ' + address.text;
+            else street = address.text;
+          }
+        });
 
         onChange({
           label: place.place_name,
           value: {
             street,
             city,
-            region,
+            region: region || city,
             countryCode: countryCode.toUpperCase(),
             longitude: place.geometry.coordinates[0],
             latitude: place.geometry.coordinates[1],
