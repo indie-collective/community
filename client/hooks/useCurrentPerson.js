0aff1d3cabde7b9bb4714cb44d73d5caef51d400
Alexis Launay <yorunohikage@gmail.com>
2021-07-28 22:08:23 +0200

    Fix unauthenticated page blink when navigating

--- client/hooks/useCurrentPerson.js
+++ client/hooks/useCurrentPerson.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import { useRouter } from 'next/router';
 
 const currentPersonQuery = gql`
   {
@@ -13,9 +14,15 @@ const currentPersonQuery = gql`
 `;
 
 export default function useCurrentPerson() {
-  const { data } = useQuery(currentPersonQuery, {
-    fetchPolicy: 'network-only',
+  const { push } = useRouter();
+  const { data, loading, error } = useQuery(currentPersonQuery, {
+    fetchPolicy: 'cache-and-network',
   });
+
+  if (!loading && error) {
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    push('/login');
+  }
 
   return (data && data.currentPerson) || null;
 }
