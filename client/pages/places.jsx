cc56cba2a4099e73b5803caa4ed1a1921acad005
Alexis Launay <yorunohikage@gmail.com>
2021-08-15 18:08:08 +0200

    Places: Display logo on markers

--- client/pages/places.jsx
+++ client/pages/places.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 import {
+  chakra,
   Badge,
   Box,
   Flex,
@@ -12,13 +13,18 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 import Head from 'next/head';
-import { Map, Marker, Overlay, ZoomControl } from 'pigeon-maps';
+import { Map, Overlay, ZoomControl } from 'pigeon-maps';
 
 import Error from './_error';
 import { withApollo } from '../lib/apollo';
 import Navigation from '../components/Navigation';
 import OrgCard from '../components/OrgCard';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+
+const TYPES_COLORS = {
+  STUDIO: 'yellow',
+  ASSOCIATION: 'green',
+};
 
 const getCitiesQuery = gql`
   ${OrgCard.fragments.org}
@@ -100,7 +106,7 @@ const Cities = () => {
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   }, [typeof window]);
 
-  const cities = data ? data.cities : {nodes: []};
+  const cities = data ? data.cities : { nodes: [] };
 
   const onMoveBand = useCallback((e) => {
     if (isDraggingBand) {
@@ -203,16 +209,56 @@ const Cities = () => {
             .map((city) => city.orgs.nodes)
             .flat()
             .map((org) => (
-              // <Overlay anchor={[location.latitude, location.longitude]}>
-              //   <Tooltip label="Hey, I'm here!" aria-label="A tooltip">
-              //     <TriangleUpIcon />
-              //   </Tooltip>
-              // </Overlay>
-              <Marker
+              <Overlay
                 key={org.id}
                 anchor={[org.location.latitude, org.location.longitude]}
-                color={org.type === 'STUDIO' ? 'red' : 'green'}
-              />
+                offset={[48 / 2, 48]}
+              >
+                <Tooltip label={org.name} aria-label="A tooltip">
+                  <Box width={50} height={50} position="relative">
+                    <chakra.svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="48px"
+                      height="48px"
+                      viewBox="0 0 480 480"
+                      color={TYPES_COLORS[org.type] + '.500'}
+                    >
+                      <defs>
+                        <mask id="mask">
+                          <rect
+                            x="0"
+                            y="0"
+                            width="480"
+                            height="480"
+                            fill="white"
+                          />
+                          <circle cx="240" cy="200" r="120" fill="black" />
+                        </mask>
+                      </defs>
+
+                      <path
+                        d="M175 429 c-63 -60 -108 -126 -126 -187 -38 -129 104 -269 235 -232 85 24 155 108 156 188 0 55 -48 141 -120 215 -36 37 -71 67 -78 67 -7 0 -38 -23 -67 -51z"
+                        fill="currentColor"
+                        mask="url(#mask)"
+                      />
+                      {org.logo && (
+                        <foreignObject x="80" y="40" width="320" height="320">
+                          <img
+                            style={{
+                              backgroundColor: 'currentColor',
+                              objectFit: 'cover',
+                              borderRadius: '50%',
+                              width: '320px',
+                              height: '320px',
+                            }}
+                            src={org.logo.thumbnail_url}
+                          />
+                        </foreignObject>
+                      )}
+                    </chakra.svg>
+                  </Box>
+                </Tooltip>
+              </Overlay>
             ))}
         </Map>
 
