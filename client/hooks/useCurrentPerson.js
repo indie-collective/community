bf7ce16860f65b539c1cfa9fc0562f636ba46c1b
Alexis Launay <yorunohikage@gmail.com>
2021-08-20 23:20:55 +0200

    Remove redirection in wrong hook

--- client/hooks/useCurrentPerson.js
+++ client/hooks/useCurrentPerson.js
@@ -1,5 +1,4 @@
 import { gql, useQuery } from '@apollo/client';
-import { useRouter } from 'next/router';
 
 const currentPersonQuery = gql`
   {
@@ -14,14 +13,12 @@ const currentPersonQuery = gql`
 `;
 
 export default function useCurrentPerson() {
-  const { push } = useRouter();
   const { data, loading, error } = useQuery(currentPersonQuery, {
     fetchPolicy: 'cache-and-network',
   });
 
   if (!loading && error) {
     document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
-    push('/login');
   }
 
   return (data && data.currentPerson) || null;
