f3835709d3ab4c5f80d29d2a1b6068e52f52eeef
Christopher Humphries <engleek@gmail.com>
2022-10-22 21:56:56 +0200

    feat: add associations and studios pages

---
+++ app/routes/studios.jsx
@@ -0,0 +1,117 @@
+import { Box, Grid, Button, Fade } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { Link, useLoaderData } from '@remix-run/react';
+import { json } from '@remix-run/node';
+import { Suspense } from 'react';
+
+import { db } from '../utils/db.server';
+import { authenticator } from '../utils/auth.server';
+import computeOrg from '../models/org';
+import OrgCard, { OrgCardSkeleton } from '../components/OrgCard';
+
+const PAGE_SIZE = 50;
+
+export const loader = async ({ request }) => {
+  const { searchParams } = new URL(request.url);
+  const page = Number(searchParams.get('page') || '1');
+
+  const currentUser = await authenticator.isAuthenticated(request);
+
+  const studios = await db.entity
+    .findMany({
+      where: {
+        type: 'studio',
+      },
+      include: {
+        location: true,
+        logo: true,
+      },
+      orderBy: {
+        updated_at: 'desc',
+      },
+      take: PAGE_SIZE,
+    })
+    .then((studios) => studios.map(computeOrg));
+
+  const data = {
+    studios: await Promise.all(studios),
+    currentUser,
+  };
+
+  // return deferred(data);
+  return json(data);
+};
+
+export const meta = () => ({
+  title: 'Studios',
+  description:
+    'Video game studios around you and all over the world.',
+  'og:title': 'Studios',
+  'og:description':
+    'Video game studios around you and all over the world.',
+  'twitter:card': 'summary',
+  'twitter:site': '@IndieColle',
+  'twitter:title': 'Studios',
+  'twitter:description':
+    'Video game studios around you and all over the world.',
+});
+
+const OrgsList = () => {
+  // const studios = useAsyncValue();
+  const { studios = [] } = useLoaderData();
+
+  return studios.map((studio) => (
+    <Box key={studio.id} minW={0}>
+      <Fade in>
+        <OrgCard {...studio} />
+      </Fade>
+    </Box>
+  ));
+};
+
+const Studios = () => {
+  const { studios, currentUser } = useLoaderData();
+
+  return (
+    <Box p={5}>
+      {currentUser && (
+        <Box textAlign="center">
+          <Button
+            as={Link}
+            to="/orgs/create"
+            mb={10}
+            size="lg"
+            colorScheme="teal"
+            leftIcon={<AddIcon />}
+          >
+            Add a studio
+          </Button>
+        </Box>
+      )}
+
+      <Grid
+        gap={5}
+        templateColumns={[
+          '1fr',
+          'repeat(2, 1fr)',
+          'repeat(3, 1fr)',
+          'repeat(4, 1fr)',
+        ]}
+      >
+        <Suspense
+          fallbackElement={[...new Array(PAGE_SIZE)].map((_, i) => (
+            <Box key={i} minW={0}>
+              <OrgCardSkeleton />
+            </Box>
+          ))}
+        >
+          {/* <Await resolve={studios}> */}
+          <OrgsList />
+          {/* </Await> */}
+        </Suspense>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Studios;
