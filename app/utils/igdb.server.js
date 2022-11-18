81c88c4b3c91577ce42967eeab52fa86a7361060
Alexis Launay <yorunohikage@gmail.com>
2022-11-19 00:39:06 +0100

    Add IGDB token rotation and cache

--- app/utils/igdb.server.js
+++ app/utils/igdb.server.js
@@ -1,15 +1,47 @@
-import createIGDBClient from "igdb-api-node";
+import cache from 'memory-cache';
+import igdb from 'igdb-api-node';
 
-export const igdb = createIGDBClient(
-  process.env.IGDB_CLIENT_ID,
-  process.env.IGDB_APP_ACCESS_TOKEN
-);
+async function refreshAccessToken(errorIfFail = false) {
+  try {
+    const result = await fetch(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`,
+      {
+        method: 'POST',
+      }
+    );
+
+    const data = await result.json();
+
+    return data;
+  } catch (err) {
+    console.log(err);
+
+    if (errorIfFail) throw err;
+
+    // retrying
+    return refreshAccessToken(true);
+  }
+}
+
+async function getAppAccessToken() {
+  const token = cache.get('igdb-access-token');
+
+  if (token) return token;
+
+  console.log('No promise to access token found in cache. Fetching new one...');
+
+  return cache.put('igdb-access-token', refreshAccessToken(), 2147483647);
+}
 
 export async function getIGDBGame(slug) {
   try {
+    const { access_token: token } = await getAppAccessToken();
+
+    console.log(`IGDB: Fetching '${slug}'.`);
+
     const {
       data: [igdbGame],
-    } = await igdb
+    } = await igdb(process.env.IGDB_CLIENT_ID, token)
       .fields([
         'name',
         'status',
@@ -21,11 +53,19 @@ export async function getIGDBGame(slug) {
         'involved_companies.company.*',
       ])
       .where(`slug = "${slug}"`)
-      .request('/games')
+      .request('/games');
 
-      return igdbGame;
+    if (!igdbGame) {
+      console.log(`IGDB: No game found for '${slug}'.`)
+      return undefined;
+    }
+
+    return {
+      ...igdbGame,
+      videos: igdbGame.videos || [], // weird, no videos -> no array
+    };
   } catch (error) {
-    console.log(`IGDB error (${slug})`, error.message);    
+    console.log(`IGDB: Error when loading ${slug}`, error.message);
   }
 
   return undefined;
