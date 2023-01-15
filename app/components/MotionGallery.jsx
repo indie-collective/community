0643712f7d06a6d1871daf3e249561034f67b66c
Alexis Launay <yorunohikage@gmail.com>
2023-01-15 16:22:05 +0100

    Fix elements on top of the gallery by using <Portal>

--- app/components/MotionGallery.jsx
+++ app/components/MotionGallery.jsx
@@ -5,6 +5,7 @@ import {
   AspectRatio,
   Link as ChakraLink,
   IconButton,
+  Portal,
 } from '@chakra-ui/react';
 import { DeleteIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
@@ -77,53 +78,55 @@ const MotionGallery = ({ gameId, images, currentUser, fetcher }) => {
         )}
       </Grid>
 
-      <Box
-        position="fixed"
-        top={0}
-        left={0}
-        bottom={0}
-        right={0}
-        background="black"
-        transition="opacity .5s ease-out"
-        onClick={() => setSelectedId(0)}
-        opacity={selectedId > 0 ? 0.5 : 0}
-        visibility={selectedId > 0 ? 'visible' : 'hidden'}
-      />
+      <Portal>
+        <Box
+          position="fixed"
+          top={0}
+          left={0}
+          bottom={0}
+          right={0}
+          background="black"
+          transition="opacity .5s ease-out"
+          onClick={() => setSelectedId(0)}
+          opacity={selectedId > 0 ? 0.5 : 0}
+          visibility={selectedId > 0 ? 'visible' : 'hidden'}
+        />
 
-      <Box
-        position="fixed"
-        top="15px"
-        left="15px"
-        bottom="15px"
-        right="15px"
-        pointerEvents="none"
-      >
-        <AnimatePresence>
-          {selectedId && (
-            <AspectRatio
-              as={motion.div}
-              ratio={16 / 9}
-              layoutId={selectedId}
-              zIndex={10000}
-              maxW="calc(100vw - 30px)"
-              maxH="calc(100vh - 30px)"
-            >
-              <Image
-                as={motion.img}
-                borderRadius="md"
-                objectFit="cover"
-                size="100%"
-                src={images[selectedId - 1].url}
-                alt=""
-                onClick={() => {
-                  setSelectedId(null);
-                }}
-                cursor="pointer"
-              />
-            </AspectRatio>
-          )}
-        </AnimatePresence>
-      </Box>
+        <Box
+          position="fixed"
+          top="15px"
+          left="15px"
+          bottom="15px"
+          right="15px"
+          pointerEvents="none"
+        >
+          <AnimatePresence>
+            {selectedId && (
+              <AspectRatio
+                as={motion.div}
+                ratio={16 / 9}
+                layoutId={selectedId}
+                zIndex={10000}
+                maxW="calc(100vw - 30px)"
+                maxH="calc(100vh - 30px)"
+              >
+                <Image
+                  as={motion.img}
+                  borderRadius="md"
+                  objectFit="cover"
+                  size="100%"
+                  src={images[selectedId - 1].url}
+                  alt=""
+                  onClick={() => {
+                    setSelectedId(null);
+                  }}
+                  cursor="pointer"
+                />
+              </AspectRatio>
+            )}
+          </AnimatePresence>
+        </Box>
+      </Portal>
     </>
   );
 };
