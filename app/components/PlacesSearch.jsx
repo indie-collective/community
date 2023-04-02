aad022d604a8dc16490254f2c31df3dca0eb73da
Alexis Launay <yorunohikage@gmail.com>
2023-04-02 22:28:03 +0200

    Fix missing LocationIcon import

--- app/components/PlacesSearch.jsx
+++ app/components/PlacesSearch.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import { LocationIcon } from './LocationIcon';
 import MapboxAutocomplete from './MapboxAutocomplete';
 
 const propTypes = {
