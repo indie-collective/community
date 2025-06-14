3c2e96bc8633456541496cfd55a8b2b1354d6d06
Christopher Humphries <engleek@gmail.com>
2025-06-14 13:11:06 +0200

    Persist IGDB cache in Postgres

--- app/utils/igdb.server.js
+++ app/utils/igdb.server.js
@@ -1,5 +1,8 @@
 import cache from 'memory-cache';
 import igdb from 'igdb-api-node';
+import { db } from './db.server';
+
+const GAME_CACHE_TTL = 1000 * 60 * 60 * 24 * 14; // 14 days
 
 async function refreshAccessToken(errorIfFail = false) {
   try {
@@ -34,6 +37,24 @@ async function getAppAccessToken() {
 }
 
 export async function getIGDBGame(slug) {
+  // Try to return cached version first to reduce calls to IGDB
+  const cachedGame = cache.get(`igdb-game-${slug}`);
+  if (cachedGame) {
+    console.log(`IGDB: Cache hit for '${slug}'.`);
+    return cachedGame;
+  }
+
+  // Fallback to database cache
+  const dbCached = await db.igdb_cache.findUnique({ where: { slug } });
+  if (
+    dbCached &&
+    Date.now() - new Date(dbCached.updated_at ?? dbCached.created_at).getTime() < GAME_CACHE_TTL
+  ) {
+    console.log(`IGDB: DB cache hit for '${slug}'.`);
+    cache.put(`igdb-game-${slug}`, dbCached.data, GAME_CACHE_TTL);
+    return dbCached.data;
+  }
+
   try {
     const { access_token: token } = await getAppAccessToken();
 
@@ -60,11 +81,21 @@ export async function getIGDBGame(slug) {
       return undefined;
     }
 
-    return {
+    const result = {
       ...igdbGame,
       videos: igdbGame.videos || [], // no videos -> no array
       screenshots: igdbGame.screenshots || [], // no screenshots -> no array
     };
+
+    // Cache the game data
+    cache.put(`igdb-game-${slug}`, result, GAME_CACHE_TTL);
+    await db.igdb_cache.upsert({
+      where: { slug },
+      create: { slug, data: result },
+      update: { data: result, updated_at: new Date() },
+    });
+
+    return result;
   } catch (error) {
     console.log(`IGDB: Error when loading ${slug}`, error.message);
   }
