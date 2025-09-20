b2ca38e6d830002688de019242e3d2fade584bbc
Christopher Humphries <engleek@gmail.com>
2025-09-20 13:23:06 +0200

    Fix AddMenuButton component

--- app/components/AddMenuButton.jsx
+++ app/components/AddMenuButton.jsx
@@ -6,30 +6,30 @@ import {
   IconButton,
 } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
-import { Link } from '@remix-run/react';
+import { useNavigate } from '@remix-run/react';
 
-const AddMenuButton = () => (
-  <Menu>
-    <MenuButton
-      as={IconButton}
-      icon={<AddIcon w={6} h={6} />}
-      size="lg"
-      colorScheme="teal"
-      variant="solid"
-      aria-label="Add new"
-    />
-    <MenuList>
-      <MenuItem as={Link} to="/games/create">
-        Add game
-      </MenuItem>
-      <MenuItem as={Link} to="/events/create">
-        Add event
-      </MenuItem>
-      <MenuItem as={Link} to="/orgs/create">
-        Add organisation
-      </MenuItem>
-    </MenuList>
-  </Menu>
-);
+const AddMenuButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        icon={<AddIcon w={6} h={6} />}
+        size="lg"
+        colorScheme="teal"
+        variant="solid"
+        aria-label="Add new"
+      />
+      <MenuList>
+        <MenuItem onClick={() => navigate('/games/create')}>Add game</MenuItem>
+        <MenuItem onClick={() => navigate('/events/create')}>Add event</MenuItem>
+        <MenuItem onClick={() => navigate('/orgs/create')}>
+          Add organisation
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
 
 export default AddMenuButton;
