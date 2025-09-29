d8248ac9b5ca04c85ba224a6fac3108869167399
Christopher Humphries <engleek@gmail.com>
2025-09-29 22:14:44 +0200

    Remove delete case from trigger function

--- prisma/migrations/20250921000000_new_game_change_approach/migration.sql
+++ prisma/migrations/20250921000000_new_game_change_approach/migration.sql
@@ -4,9 +4,7 @@ ALTER TABLE "game" ADD COLUMN "lastModifiedById" UUID;
 -- UpdateTriggerFunction
 CREATE OR REPLACE FUNCTION process_game_change() RETURNS TRIGGER AS $game_change$
     BEGIN
-        IF (TG_OP = 'UPDATE' AND NEW.deleted IS TRUE) THEN
-            INSERT INTO game_change (operation, created_at, author_id, game_id, name, about, igdb_slug, site) SELECT 'delete', now(), OLD.lastModifiedById, OLD.id, OLD.name, OLD.about, OLD.igdb_slug, OLD.site;
-        ELSIF (TG_OP = 'UPDATE') THEN
+        IF (TG_OP = 'UPDATE') THEN
             insert into game_change (operation, created_at, author_id, game_id, name, about, igdb_slug, site) select 'update', now(), NEW.lastModifiedById, NEW.id, NEW.name, NEW.about, NEW.igdb_slug, NEW.site;
         ELSIF (TG_OP = 'INSERT') THEN
             INSERT INTO game_change (operation, created_at, author_id, game_id, name, about, igdb_slug, site) SELECT 'create', now(), NEW.lastModifiedById, NEW.id, NEW.name, NEW.about, NEW.igdb_slug, NEW.site;
