ac19151af08fba64e6198f0a681d73c8b0bbbc5c
Christopher Humphries <engleek@gmail.com>
2025-12-25 23:55:32 +0100

    feat: default new dev users to admin status upon creation

--- app/utils/auth.server.js
+++ app/utils/auth.server.js
@@ -51,6 +51,7 @@ if (process.env.NODE_ENV === 'development') {
               email,
               username,
               first_name: username,
+              isAdmin: true,
             },
           });
         }
