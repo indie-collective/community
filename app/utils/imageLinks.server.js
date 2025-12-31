d9e8f5a986b0ac5db7869e723de8abb7cae837a9
Christopher Humphries <engleek@gmail.com>
2025-12-31 17:58:10 +0100

    Handle absolute URLs in getImageLinks utility
    Updated getImageLinks to return direct URLs when the image file name is an absolute URL (starts with 'http'), bypassing CDN host construction. This allows support for externally hosted images.

--- app/utils/imageLinks.server.js
+++ app/utils/imageLinks.server.js
@@ -1,7 +1,15 @@
 export default function getImageLinks(image) {
+  const name = image.image_file.name;
+  if (name?.startsWith('http')) {
+    return {
+      id: image.id,
+      url: name,
+      thumbnail_url: name,
+    };
+  }
   return {
     id: image.id,
-    url: `https://${process.env.CDN_HOST}/${image.image_file.name}`,
-    thumbnail_url: `https://${process.env.CDN_HOST}/thumb_${image.image_file.name}`,
+    url: `https://${process.env.CDN_HOST}/${name}`,
+    thumbnail_url: `https://${process.env.CDN_HOST}/thumb_${name}`,
   };
 }
